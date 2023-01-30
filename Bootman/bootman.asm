d06148367fe7a3b076b5b37671e2bfd807c2ccbd
TheGrandNoob <56801098+TheGrandNoob@users.noreply.github.com>
2023-01-30 10:26:44 +0300

    Build v0.12
    Shell added

--- Bootman/bootman.asm
+++ Bootman/bootman.asm
@@ -10,26 +10,42 @@ HelloWorldStr db "HelloWorld$"
 main:
 
         call disks_init
+        
         call int21h_init
 
         in al, 0x92
         or al, 2
         out 0x92, al
         
-        mov ah , 9
-        mov dx , HelloWorldStr
-        int 21h
-        jmp $
+
+        call shell_task
+
+        call poweroff
 
 
+poweroff:
 
-bootmanLoop:
+        mov     ax, 5301h
+        xor     bx, bx
+        int     15h
 
-        jmp bootmanLoop
+        ;try to set APM version (to 1.2)
+        mov     ax, 530Eh
+        mov     cx, 0102h
+        xor     bx, bx
+        int     15h
+
+        ;turn off the system
+        mov     ax, 5307h
+        mov     bx, 0001h
+        mov     cx, 0003h
+        int     15h
+        hlt
+        ret
 
 include 'commondata.inc'
 include 'int21h.inc'
 include 'disks.inc'
-
+include 'shell.inc'
 align 16
 DISK_RW_BUFFER:
